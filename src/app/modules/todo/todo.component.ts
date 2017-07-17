931adeb97010299027656d7c0c5a265dc6bee2fc
SisiNiSawa <SisiNiSawa@users.noreply.github.com>
2017-07-17 00:31:25 -0400

    todo list v1 mostly completed, need to fix bug with checking items

--- src/app/modules/todo/todo.component.ts
+++ src/app/modules/todo/todo.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidebarService } from '../../sidebar/sidebar.service';
+import { DatabaseService } from '../../database.service';
+
+import { Todo } from '../../shared/todo.model';
+import { TodoItem } from '../../shared/todo-item.model';
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  checkmark: boolean = false;
+  todo: Todo;
+  editIndex: number = undefined;
+
+  constructor(
+    private sidebarService: SidebarService,
+    private dbService: DatabaseService
+  ) {
+    this.todo = this.sidebarService.selectedItem;
+  }
 
   ngOnInit() {
+  }
+
+  newItem() {
+    let newTodo = new TodoItem;
+    newTodo.order = this.todo.items.length + 1;
+    this.todo.items.push(newTodo);
+    this.pushToDB()
+    setTimeout( () => {
+      this.editIndex = this.todo.items.length -1;
+    }, 10)
+  }
+
+  removeItem(index: number) {
+    this.editIndex = undefined;
+    this.todo.items.splice(index, 1);
+    this.pushToDB()
+  }
+
+  editItem(index: number) {
+    this.editIndex = index;
+  }
+
+  updateItem(index: number) {
+    this.editIndex = undefined;
+    this.pushToDB()
+  }
+
+  toggleChecked(index: number) {
+    this.todo.items[index].checked = !this.todo.items[index].checked;
+    this.pushToDB();
+  }
+
+  // so we always have an up to date revision for our todo list
+  pushToDB() {
+    this.dbService.updateItem(this.todo).then( () => {
+      this.dbService.getEntryByID(this.todo._id).then( (todo) => {
+        this.todo = todo;
+      });
+    });
   }
 
 }
